c9af043708c4168f7f1c1cbe4130fe476d70516b
Tallerik <erik.klasse@gmail.com>
2019-03-23 21:02:09 +0100

    Fixes

--- app/src/main/java/de/tallerik/bas_vertretung/ScrollingActivity.java
+++ app/src/main/java/de/tallerik/bas_vertretung/ScrollingActivity.java
@@ -4,14 +4,12 @@ import android.content.Intent;
 import android.net.Uri;
 import android.os.Bundle;
 import android.support.design.widget.FloatingActionButton;
-import android.support.design.widget.Snackbar;
 import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.Toolbar;
 import android.view.Menu;
 import android.view.MenuItem;
 import android.view.View;
 import android.widget.LinearLayout;
-import android.widget.TableLayout;
 import android.widget.TextView;
 
 import org.json.JSONArray;
@@ -23,11 +21,10 @@ public class ScrollingActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_scrolling);
-        Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
+        Toolbar toolbar = findViewById(R.id.toolbar);
         setSupportActionBar(toolbar);
 
-        FloatingActionButton fab = (FloatingActionButton) findViewById(R.id.fab);
-        fab.setImageDrawable(getResources().getDrawable(R.drawable.ic_bas));
+        FloatingActionButton fab = findViewById(R.id.fab);
         fab.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
@@ -109,7 +106,6 @@ public class ScrollingActivity extends AppCompatActivity {
 
         JSONArray morgen = json.getJSONArray("morgen");
 
-        TextView morgentext = new TextView(this);
 
         for (int i = 0; i < morgen.length(); i++) {
             JSONArray array = morgen.getJSONArray(i);
